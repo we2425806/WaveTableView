ac5bac8686a099e0c3ba763099818c7549cfc0fe
京 <@163.com>
2016-11-06 18:53:49 +0800

    创建项目

---
+++ WaveTableview/WaveTableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WaveTableview
+//
+//  Created by apple on 16/11/6.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WaveTableview/WaveTableview/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WaveTableview
+//
+//  Created by apple on 16/11/6.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
